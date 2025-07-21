30f82e497a33da230817160e1dd50cc9a09b6d90
Sreeja-dev14 <sreejadevulapally0@gmail.com>
2025-07-21 09:11:37 +0530

    Add files via upload

---
+++ RecordManagement/urls.py
@@ -0,0 +1,34 @@
+"""CrimeRecordManagement URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from predictor.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    path('', user_login, name='login'),  # Root URL is login
+    path('login/', user_login, name='login'),
+    path('home/', home, name='home'),
+    path('heart/', heart, name='heart'),
+    path('pimadiabetes/', pimadiabetes, name='pimadiabetes'),
+    path('breast/', breast, name='breast'),
+    path('diabetic/', diabetic, name='diabetic'),
+    path('retinopathy/', retinopathy, name='retinopathy'),
+
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
